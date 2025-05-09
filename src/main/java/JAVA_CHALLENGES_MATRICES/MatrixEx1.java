0ec68756988bd8fe1709f9013fe1c75261df5964
cvega <carlosjuniorrvega@hotmail.com>
2025-05-08 21:29:26 -0500

    chore: add format to code. CV

--- src/main/java/JAVA_CHALLENGES_MATRICES/MatrixEx1.java
+++ src/main/java/JAVA_CHALLENGES_MATRICES/MatrixEx1.java
@@ -10,37 +10,34 @@ solucion 1.
 
  */
 
-//Solucion 1
-//public class MatrixEx1 {
-//    public static int sumarElementos( int[][] matriz){
-//        int acumulador = 0;
-//
-//        for(int i =0; i < matriz.length; i++){
-//            for(int j =0; j < matriz[i].length; j++){
-//                acumulador+=matriz[i][j];
-//            }
-//
-//        }
-//        System.out.println(acumulador);
-//        return acumulador;
-//
-//    }
-//
-//
-//    public static void main(String[] args){
-//
-//       int[][] matriz = {{1,2,3},{4,5,6},{7,8,9}};
-//       sumarElementos(matriz);
-//
-//    }
-//}
+/*Solucion 1
+public class MatrixEx1 {
+    public static int sumarElementos( int[][] matriz){
+        int acumulador = 0;
+
+        for(int i =0; i < matriz.length; i++){
+            for(int j =0; j < matriz[i].length; j++){
+                acumulador+=matriz[i][j];
+            }
+
+        }
+        System.out.println(acumulador);
+        return acumulador;
+
+    }
 
 
+    public static void main(String[] args){
+
+       int[][] matriz = {{1,2,3},{4,5,6},{7,8,9}};
+       sumarElementos(matriz);
+
+    }
+}*/
 
 
-
-//solucion 2
-/*public class MatrixEx1{
+/*solucion 2
+public class MatrixEx1{
     public static int sumar(int[][] arreglo){
         int acumulador =0;
         for(int i =0; i < arreglo.length;i++){
@@ -57,11 +54,11 @@ solucion 1.
 
         int [][] matriz = new int[filas][columnas];
         //verificamos la matriz creada
-//        for (int i =0; i < matriz.length;i++){
-//            for(int j=0; j< matriz[i].length;j++){
-//                System.out.println(matriz[i][j]);
-//            }
-//        }
+     for (int i =0; i < matriz.length;i++){
+            for(int j=0; j< matriz[i].length;j++){
+                System.out.println(matriz[i][j]);
+            }
+        }
 
         //llenamos la matriz
         for(int i =0; i < matriz.length;i++){
@@ -86,53 +83,41 @@ solucion 1.
 }*/
 
 
-
 //solucion 3
 
 import java.util.ArrayList;
 
-public class MatrixEx1{
+public class MatrixEx1 {
 
     //Creamos la funcion
     public static int sumarElementos(ArrayList<ArrayList<Integer>> arreglo) {
         int acumulador = 0;
 
         for (int i = 0; i < arreglo.size(); i++) {
-            for (int j = 0; j< arreglo.get(i).size(); j++) {
+            for (int j = 0; j < arreglo.get(i).size(); j++) {
                 acumulador += arreglo.get(i).get(j);
                 System.out.println(acumulador);
             }
-
         }
         return acumulador;
-
     }
 
-
-
-
-
-
-
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         //creamos el arrylist
         ArrayList<ArrayList<Integer>> myArray = new ArrayList<ArrayList<Integer>>();
-
-        int filas = (int) Math.floor(Math.random()*10) +1;
-        int columnas = (int) Math.floor(Math.random()*10) +1;
-
-        for(int i=0; i < filas;i++){
+        int filas = (int) Math.floor(Math.random() * 10) + 1;
+        int columnas = (int) Math.floor(Math.random() * 10) + 1;
+        for (int i = 0; i < filas; i++) {
             myArray.add(new ArrayList<Integer>());
-            for(int j=0; j < columnas;j++){
-                int valor = (int) Math.floor(Math.random()*10);
+            for (int j = 0; j < columnas; j++) {
+                int valor = (int) Math.floor(Math.random() * 10);
                 myArray.get(i).add(valor);
             }
         }
+
         //mostramos todos los elementos del arreglo creado
-        for(int i =0; i < myArray.size();i++){
-            for (int j=0; j<myArray.get(i).size();j++ ){
+        for (int i = 0; i < myArray.size(); i++) {
+            for (int j = 0; j < myArray.get(i).size(); j++) {
                 System.out.println(myArray.get(i).get(j));
             }
         }
