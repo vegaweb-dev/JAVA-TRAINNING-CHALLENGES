dad6d41a6d919b0fddd5830ca2f5573192822e65
cvega <carlosjuniorrvega@hotmail.com>
2025-08-15 23:20:49 -0500

    feat(JAVA_PROGRESSIVE_CHALLENGES): solve Ex11_IsPrimeOrNot .CV

---
+++ src/main/java/JAVA_PROGRESSIVE_CHALLENGES/Ex11_IsPrimeOrNot.java
@@ -0,0 +1,70 @@
+package JAVA_PROGRESSIVE_CHALLENGES;
+
+//Create a Java program that determines whether an entered number is prime or not prime.
+//Requirements:
+//There must be a POJO class to store the response ("is prime" or "is not prime").
+//Implement a Calculator class with a method to check if a number is prime.
+//Use a method that returns an object of the POJO class with the response.
+//Solution:
+
+class ValueEx11 {
+    private int value;
+
+    ValueEx11(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+class CalculatorEx11 {
+    ValueEx11 valueEx11;
+
+    CalculatorEx11(ValueEx11 valueEx11) {
+        this.valueEx11 = valueEx11;
+    }
+
+    boolean isPrimeOrNot() {
+        if (valueEx11.getValue() < 2) {
+            return false;
+        }
+        if (valueEx11.getValue() == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(valueEx11.getValue()); i++) {
+            if (valueEx11.getValue() % i == 0) {
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+
+}
+
+class PojoForValues {
+    CalculatorEx11 calculatorEx11;
+
+    PojoForValues(CalculatorEx11 calculatorEx11) {
+        this.calculatorEx11 = calculatorEx11;
+    }
+
+    boolean getRespose(CalculatorEx11 calculatorEx11) {
+        System.out.println(calculatorEx11.isPrimeOrNot());
+        return calculatorEx11.isPrimeOrNot();
+    }
+}
+
+
+public class Ex11_IsPrimeOrNot {
+    public static void main(String[] args) {
+        ValueEx11 valueEx11 = new ValueEx11(6);
+        CalculatorEx11 calculatorEx11 = new CalculatorEx11(valueEx11);
+        PojoForValues pojoForValues = new PojoForValues(calculatorEx11);
+        pojoForValues.getRespose(calculatorEx11);
+    }
+
+}
